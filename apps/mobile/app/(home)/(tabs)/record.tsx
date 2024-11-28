d7ac621150f53ca2d84b1bc496441c165aa1fcfe
Thomas Gak-Deluen <tgdn45@gmail.com>
2024-11-28 11:29:56 +0100

    Improved recording screen

--- apps/mobile/app/(home)/(tabs)/record.tsx
+++ apps/mobile/app/(home)/(tabs)/record.tsx
@@ -1,39 +1,70 @@
 import React from "react";
-import { StyleSheet, ScrollView } from "react-native";
+import { Text, View } from "react-native";
+import { createStyleSheet, useStyles } from "react-native-unistyles";
 
-// import EditScreenInfo from "@/components/EditScreenInfo";
-import { View } from "@/components/Themed";
 import { RecordButton } from "@/components/RecordButton";
+import { useRecordingContext } from "@/providers/recording.provider";
 
 export default function TabTwoScreen() {
+  const { styles, theme } = useStyles(stylesheet);
+  const { audioRecorder, startRecording, stopRecording } =
+    useRecordingContext();
+  const recordingDuration = audioRecorder.currentTime;
+  console.log({ recordingDuration });
+  React.useEffect(() => {
+    const intervalId = setInterval(() => {
+      console.log({ recordingDuration });
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        <View
-          style={styles.separator}
-          lightColor="#eee"
-          darkColor="rgba(255,255,255,0.1)"
-        />
-        <RecordButton />
-        {/* <EditScreenInfo path="app/(tabs)/two.tsx" /> */}
+    <View style={styles.container}>
+      <View style={styles.contentContainer}>
+        <Text
+          style={{
+            fontSize: 70,
+            fontWeight: "300",
+            color: theme.colors.typography,
+          }}
+        >
+          00:00:00
+        </Text>
       </View>
-    </ScrollView>
+      <View
+        style={{
+          paddingHorizontal: 10,
+          paddingVertical: 10,
+          marginBottom: 40,
+          alignItems: "center",
+        }}
+      >
+        <RecordButton
+          onPress={() => {
+            if (audioRecorder.isRecording) {
+              stopRecording();
+            } else {
+              startRecording();
+            }
+          }}
+        />
+      </View>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
+const stylesheet = createStyleSheet((theme) => ({
   container: {
     flex: 1,
-    alignItems: "center",
+    backgroundColor: theme.colors.backgroundColor,
+    paddingHorizontal: theme.spacing.screenGutter,
+  },
+  contentContainer: {
+    flex: 1,
     justifyContent: "center",
+    alignItems: "center",
   },
   title: {
     fontSize: 20,
     fontWeight: "bold",
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
+}));
--- apps/mobile/components/RecordButton.tsx
+++ apps/mobile/components/RecordButton.tsx
@@ -1,61 +1,96 @@
 import * as React from "react";
-import { Alert, Button, TouchableOpacity } from "react-native";
+import {
+  Alert,
+  Button,
+  Pressable,
+  View,
+  type PressableProps,
+} from "react-native";
+import { createStyleSheet, useStyles } from "react-native-unistyles";
 import { Audio } from "expo-av";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
-import { Text, View } from "@/components/Themed";
+export function RecordButton(props: PressableProps) {
+  const { styles } = useStyles(stylesheet);
+  // const [recording, setRecording] = React.useState<
+  //   Audio.Recording | undefined
+  // >();
+  // const isRecording = !!recording;
+  // const [permissionResponse, requestPermission] = Audio.usePermissions();
+  // async function startRecording() {
+  //   console.log("Starting recording..");
+  //   // TODO: debugging comment to prevent recording
+  //   return;
+  //   try {
+  //     if (permissionResponse?.status !== Audio.PermissionStatus.GRANTED) {
+  //       console.log("Requesting permission..");
+  //       await requestPermission();
+  //     }
+  //     await Audio.setAudioModeAsync({
+  //       allowsRecordingIOS: true,
+  //       playsInSilentModeIOS: true,
+  //     });
 
-export function RecordButton() {
-  const [recording, setRecording] = React.useState<
-    Audio.Recording | undefined
-  >();
-  const [permissionResponse, requestPermission] = Audio.usePermissions();
-
-  async function startRecording() {
-    try {
-      if (permissionResponse?.status !== Audio.PermissionStatus.GRANTED) {
-        console.log("Requesting permission..");
-        await requestPermission();
-      }
-      await Audio.setAudioModeAsync({
-        allowsRecordingIOS: true,
-        playsInSilentModeIOS: true,
-      });
-
-      console.log("Starting recording..");
-      const { recording } = await Audio.Recording.createAsync(
-        Audio.RecordingOptionsPresets.HIGH_QUALITY,
-      );
-      setRecording(recording);
-      console.log("Recording started");
-    } catch (err) {
-      console.error("Failed to start recording", err);
-      Alert.alert("Failed to start recording");
-    }
-  }
-
-  async function stopRecording() {
-    console.log("Stopping recording..");
-    setRecording(undefined);
-    await recording?.stopAndUnloadAsync();
-    await Audio.setAudioModeAsync({
-      allowsRecordingIOS: false,
-    });
-    const uri = recording?.getURI();
-    console.log("Recording stopped and stored at", uri);
-  }
+  //     console.log("Starting recording..");
+  //     const { recording } = await Audio.Recording.createAsync(
+  //       Audio.RecordingOptionsPresets.HIGH_QUALITY,
+  //     );
+  //     setRecording(recording);
+  //     console.log("Recording started");
+  //   } catch (err) {
+  //     console.error("Failed to start recording", err);
+  //     Alert.alert("Failed to start recording");
+  //   }
+  // }
+  // async function stopRecording() {
+  //   console.log("Stopping recording..");
+  //   setRecording(undefined);
+  //   await recording?.stopAndUnloadAsync();
+  //   await Audio.setAudioModeAsync({
+  //     allowsRecordingIOS: false,
+  //   });
+  //   const uri = recording?.getURI();
+  //   console.log("Recording stopped and stored at", uri);
+  // }
   return (
-    <TouchableOpacity
-      onPress={() => (recording ? stopRecording : startRecording)}
+    <Pressable
+      // onPress={() => (recording ? stopRecording : startRecording)}
+      style={styles.container}
+      {...props}
     >
-      <View>
-        <Text>{recording ? "Stop Recording" : "Start Recording"}</Text>
+      <View style={styles.innerContainer}>
+        <FontAwesome size={38} color="white" name="microphone" />
       </View>
-    </TouchableOpacity>
-  );
-  return (
-    <Button
-      title={recording ? "Stop Recording" : "Start Recording"}
-      onPress={() => (recording ? stopRecording : startRecording)}
-    />
+    </Pressable>
   );
 }
+
+const stylesheet = createStyleSheet((theme) => ({
+  container: {
+    borderRadius: 100,
+    width: 90,
+    height: 90,
+    padding: 3,
+    borderWidth: 3,
+    borderColor: theme.colors.blood,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  innerContainer: {
+    borderRadius: 100,
+    width: "100%",
+    height: "100%",
+    backgroundColor: theme.colors.blood,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+}));
